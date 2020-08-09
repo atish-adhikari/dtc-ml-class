dd5a93c9d6b5ca1762155f1bf686435082d42d21
Atish Adhikari <aadhikari@deerhold.org>
2020-08-09 09:53:19 +0545

    Django API and ajax

--- Django/my-project/ml/serializers.py
+++ Django/my-project/ml/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
 class AdmissionSerializer(serializers.Serializer):
-    gre = serializers.IntegerField(required=True)
-    toefl = serializers.IntegerField(required=True)
-    rating = serializers.FloatField(required=True)
-    sop = serializers.FloatField(required=True)
-    lor = serializers.FloatField(required=True)
-    cgpa = serializers.FloatField(required=True)
-    research = serializers.IntegerField(required=True)
+    gre = serializers.IntegerField(required=True, min_value=240, max_value=340)
+    toefl = serializers.IntegerField(required=True, min_value=50, max_value=120)
+    rating = serializers.FloatField(required=True, min_value=0, max_value=5)
+    sop = serializers.FloatField(required=True, min_value=0, max_value=5)
+    lor = serializers.FloatField(required=True, min_value=0, max_value=5)
+    cgpa = serializers.FloatField(required=True, min_value=0, max_value=10)
+    research = serializers.IntegerField(required=True, min_value=0, max_value=1)
